49f4720892b3ed070eaa079d30cf15bfc9ae47aa
Tatsu-code452 <tatsu.dragon.416@gmail.com>
2026-09-17 11:55:06 +0900

    pages

--- ts-parts/vite.config.ts
+++ ts-parts/vite.config.ts
@@ -4,5 +4,6 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     plugins: [react(), tailwindcss()],
+    base: '/ts-parts/',
     server: { port: 3002, strictPort: true },
 });
